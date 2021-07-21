fc68cc8cab9c93026ebd58f33b5ac49704f74cb4
daintyflowerss <84612691+daintyflowerss@users.noreply.github.com>
2021-07-21 21:13:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
+var score = 0
+var scissors  = "scissors"
+var stone = "stone"
+var paper = "paper"
 var main = function (input) {
-  // randomly chooses scissors, paper or stone
-  var useroption =input;
-  var option1 = "scissors";
-  var option2 = "paper";
-  var option3 = "stone";
-  if (user option == !("scissors" || "paper" || "rock")){
-    myOutputValue = 'you guessed ${input}, there are only theree options stone, paper, scissors';
-  }
-  if(
-    ( useroption == "scissors" && computeroption == "paper") ||
-    (useroption == "paper" && computeroption =="stone") ||
-    (useroption == "stone" && computeroption == "scissors")
-  ) {
-    myOutputValue = 'user won!';
-  }
-  if (
-    (computeroption == "scissors" && useroption == "paper") ||
-    (computeroption == "paper" && useroption == "stone") ||
-    (computeroption == "stone" && useroption == "scissors")
-  ) {
-    myOutputValue = 'computer won!';
-  }
-  if (useroption == computeroption) {
-    myOutputValue = 'draw';
-  }
-  return myOutputValue
-};
-
+var computerChoice = getObjectforComputer ()
+var outcome = winCondition (input, computerChoice) 
+return outcome
+}
+ 
+var winCondition = function(input, computerChoice){
+console.log(computerChoice)
+  if(input == computerChoice) {return "draw"};
+  console.log("success one")
+  if(input == scissors && computerChoice == paper || input == stone && computerChoice == scissors ||input == paper && computerChoice == stone ) {
+    score+=1
+    return "win"
+    };
+  console.log("success two")
+  if(input == scissors && computerChoice == stone || input == stone && computerChoice == paper ||input == paper && computerChoice == scissors) {
+    score-0=1
+    return "lose"
+    };
+  return "Input not valid. Please enter either Scissors, Paper, or Stone"
+}
+var getObjectforComputer = function(){
+  var number = Math.floor(Math.random() * 2);
+  console.log("success one")
+  if (number == 0){
+    return scissors;
+  };
+  console.log("a")
+  if (number == 1){
+    return paper;
+  };
+  console.log("b")
+  if (number == 2){
+    return stone
+  };
+} 
